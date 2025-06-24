dda6c117ac809610e3c916890029f1770004f4c6
Gergely Juhasz <predeimos@gmail.com>
2025-06-24 20:43:00 +0100

    Extract Libs from project

--- src/main/. sheetsLib/wrappers/inputs/CBox.js
+++
@@ -1,2 +0,0 @@
-class CBox extends Input{
-}
--- src/test/. sheetTestLib/TimeOutException.js
+++
@@ -1,6 +0,0 @@
-class TimeOutException extends Error {
-  constructor() {
-    super("Test runtime reached the treshold, shutting down and saving progress."); 
-    this.name = "TimeOutException";
-  }
-}
