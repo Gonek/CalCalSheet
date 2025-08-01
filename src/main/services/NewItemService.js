c8eea9d45ddb26bfd6112d97c24b2af43e29e22a
Gergely Juhasz <predeimos@gmail.com>
2025-08-01 09:53:47 +0100

    Minor last minute fixes

--- src/main/services/NewItemService.js
+++ src/main/services/NewItemService.js
@@ -13,7 +13,7 @@ class NewItemService {
     let noomColourRng = getRng(RNG.NEW_ITEM_NOOM_COLOUR);
     let autoDeleteRng = getRng(RNG.NEW_ITEM_AUTO_DELETE);
     this.itemRepository.addOrUpdate(new Item(itemName, fields,
-                                             prices[0], prices[1].toFixed(2),
+                                             prices[0], prices[1],
                                              noomColourRng.getDisplayValue(), 
                                              autoDeleteRng.getValue()), oldPos);
 
@@ -34,7 +34,7 @@ class NewItemService {
       let priceRng = getRng(RNG.NEW_ITEM_PRICE);
       if(item.price && item.pricePerUnit){
         priceRng.setValue(item.price, 1, 1);
-        priceRng.setValue((item.price / item.pricePerUnit).toFixed(2), 2, 1);
+        priceRng.setValue((item.price / item.pricePerUnit), 2, 1);
       }
       getRng(RNG.NEW_ITEM_AUTO_DELETE).setValue('Never');
     }
